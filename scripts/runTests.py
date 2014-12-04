528dc3d8c691a6aa71a9ab5021a243b384239c9f
Ivan Herman <ivan@ivan-herman.net>
2014-12-04 12:07:33 +0100

    Created a rudimentary test runner

---
+++ scripts/runTests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+Run the microdata testing locally
+"""
+
+import sys
+sys.path.insert(0,"/Users/ivan/Library/Python")
+
+# You may want to adapt this to your environment...
+import sys, getopt
+
+from pyMicrodata import pyMicrodata, __version__
+
+###########################################
+
+test_path      = "/Users/ivan/W3C/github/microdata-rdf/tests/"
+test_file_base = test_path + ("%04d" % int(sys.argv[1]))
+test_html = test_file_base + ".html"
+test_ttl  = test_file_base + ".ttl"
+
+processor = pyMicrodata()
+print processor.rdf_from_source(test_html)
+print "----"
+with open(test_ttl) as f :
+	for l in f:
+		print l
+
+
+
+
