b7f95506268ef5c0fcd726cac30b71f80dddf5ae
Joern Hees <dev@joernhees.de>
2016-01-27 21:34:22 +0100

    PEP8 stuff

--- scripts/CGI_microdata.py
+++ scripts/CGI_microdata.py
@@ -20,77 +20,80 @@ $Id: microdata.py,v 1.3 2012/03/06 14:53:31 ivan Exp $
 
 __version__ = "3.0"
 import cgi
-import cgitb; cgitb.enable()
+import cgitb;
+
+cgitb.enable()
 import sys, os
 import StringIO
-#cgi.print_environ()
 
-if sys.platform == "darwin" :
-	# this is my local machine
-	sys.path.insert(0,"/Users/ivan/W3C/dev/2004/PythonLib-IH")
-	sys.path.insert(0,"/Users/ivan/Library/Python")
-	sys.path.insert(0,"/Users/ivan/Source/PythonModules/pyMicrodata-1.0")
-	sys.path.insert(0,"/Users/ivan/W3C/dev/2004/PythonLib-IH/rdfa-1.1")
-else :
-	# this is the server on W3C
-	sys.path.insert(0,"/usr/local/lib/python2.4/site-packages/PythonLib-IH")
-	sys.path.insert(0,"/usr/local/lib/python2.4/site-packages/PythonLib-IH/rdfa-1.1")
+# cgi.print_environ()
+
+if sys.platform == "darwin":
+    # this is my local machine
+    sys.path.insert(0, "/Users/ivan/W3C/dev/2004/PythonLib-IH")
+    sys.path.insert(0, "/Users/ivan/Library/Python")
+    sys.path.insert(0, "/Users/ivan/Source/PythonModules/pyMicrodata-1.0")
+    sys.path.insert(0, "/Users/ivan/W3C/dev/2004/PythonLib-IH/rdfa-1.1")
+else:
+    # this is the server on W3C
+    sys.path.insert(0, "/usr/local/lib/python2.4/site-packages/PythonLib-IH")
+    sys.path.insert(0,
+                    "/usr/local/lib/python2.4/site-packages/PythonLib-IH/rdfa-1.1")
 
 from pyMicrodata import processURI
 
 #
 # to make this thing exist...
-uri  = ""
+uri = ""
 form = cgi.FieldStorage()
 # First see if an upload is present in the form
-if "uploaded" in form and form["uploaded"].file :
-	uri = "uploaded:"
-elif "text" in form and form["text"].value != None and len(form["text"].value.strip()) != 0 :
-	uri  = "text:"
-else :
-	if not "uri" in form :
-		print 'Content-type: text/html; charset=utf-8'
-		print 'Status: 400 Invalid Input'
-		print
-		print "<html>"
-		print "<head>"
-		print "<title>Error in RDFa processing</title>"
-		print "</head><body>"
-		print "<h1>Error in distilling RDFa</h1>"
-		print "No URI has been specified"
-		print "</body>"
-		print "</html>"
-		sys.exit(1)
-	
-	try :
-		#uri = form["uri"].value
-		uri = form.getfirst("uri")
-	except :
-		print 'Content-type: text/html; charset=utf-8'
-		print 'Status: 400 Invalid Input'
-		print
-		print "<html>"
-		print "<head>"
-		print "<title>Error in RDFa processing</title>"
-		print "</head><body>"
-		print "<h1>Error in distilling RDFa</h1>"
-		print "No URI has been specified"
-		print "</body>"
-		print "</html>"
-		sys.exit(1)
+if "uploaded" in form and form["uploaded"].file:
+    uri = "uploaded:"
+elif "text" in form and form["text"].value != None and len(
+        form["text"].value.strip()) != 0:
+    uri = "text:"
+else:
+    if not "uri" in form:
+        print 'Content-type: text/html; charset=utf-8'
+        print 'Status: 400 Invalid Input'
+        print
+        print "<html>"
+        print "<head>"
+        print "<title>Error in RDFa processing</title>"
+        print "</head><body>"
+        print "<h1>Error in distilling RDFa</h1>"
+        print "No URI has been specified"
+        print "</body>"
+        print "</html>"
+        sys.exit(1)
 
-if uri == "referer" :
-	uri    = os.getenv('HTTP_REFERER')
-	newuri = "http://www.w3.org/2012/pyMicrodata/extract?uri=" + uri
-	print "Status: 302 Moved"
-	print "Location: " + newuri
-	print
-else :
-	if "format" in form.keys() :
-		format = form.getfirst("format")
-	else :
-		format = "turtle"
-	retval = processURI(uri, format, form)
-	print retval
+    try:
+        # uri = form["uri"].value
+        uri = form.getfirst("uri")
+    except:
+        print 'Content-type: text/html; charset=utf-8'
+        print 'Status: 400 Invalid Input'
+        print
+        print "<html>"
+        print "<head>"
+        print "<title>Error in RDFa processing</title>"
+        print "</head><body>"
+        print "<h1>Error in distilling RDFa</h1>"
+        print "No URI has been specified"
+        print "</body>"
+        print "</html>"
+        sys.exit(1)
 
-
+if uri == "referer":
+    uri = os.getenv('HTTP_REFERER')
+    newuri = "http://www.w3.org/2012/pyMicrodata/extract?uri=" + uri
+    print "Status: 302 Moved"
+    print "Location: " + newuri
+    print
+else:
+    if "format" in form.keys():
+        format = form.getfirst("format")
+    else:
+        format = "turtle"
+    retval = processURI(uri, format, form)
+    print retval
--- scripts/runTests.py
+++ scripts/runTests.py
@@ -4,7 +4,8 @@ Run the microdata testing locally
 """
 
 import sys
-sys.path.insert(0,"/Users/ivan/Library/Python")
+
+sys.path.insert(0, "/Users/ivan/Library/Python")
 
 # You may want to adapt this to your environment...
 import sys, getopt
@@ -13,24 +14,20 @@ from pyMicrodata import pyMicrodata, __version__
 
 ###########################################
 
-test_path      = "/Users/ivan/W3C/github/microdata-rdf/tests/"
+test_path = "/Users/ivan/W3C/github/microdata-rdf/tests/"
 test_file_base = test_path + ("%04d" % int(sys.argv[1]))
-#test_file_base = test_path + ("sdo_eg_md_%d" % int(sys.argv[1]))
+# test_file_base = test_path + ("sdo_eg_md_%d" % int(sys.argv[1]))
 test_html = test_file_base + ".html"
-test_ttl  = test_file_base + ".ttl"
+test_ttl = test_file_base + ".ttl"
 
 processor = pyMicrodata()
 print processor.rdf_from_source(test_html)
 print "----"
-with open(test_ttl) as f :
-	for l in f:
-		print l,
+with open(test_ttl) as f:
+    for l in f:
+        print l,
 
 print "----"
-with open(test_html) as f :
-	for l in f:
-		print l,
-
-
-
-
+with open(test_html) as f:
+    for l in f:
+        print l,
